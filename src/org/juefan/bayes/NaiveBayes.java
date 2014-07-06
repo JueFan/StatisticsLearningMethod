a6c4ad18b16bc67982c8e55e5a5376f13a0c9b24
JueFan <juefan_c@qq.com>
2014-07-06 23:02:23 +0800

    高大上接口不会弄
    回滚

--- src/org/juefan/bayes/NaiveBayes.java
+++ src/org/juefan/bayes/NaiveBayes.java
@@ -1,19 +1,6 @@
 package org.juefan.bayes;
 
-import java.util.ArrayList;
 
-import org.juefan.basic.BasicData;
-import org.juefan.basic.ClassificationAlg;
-import org.juefan.data.Data;
-
-public class NaiveBayes implements ClassificationAlg{
-
-	@Override
-	public boolean isStop(ArrayList<? extends BasicData> datas) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
+public class NaiveBayes{
 
 }
