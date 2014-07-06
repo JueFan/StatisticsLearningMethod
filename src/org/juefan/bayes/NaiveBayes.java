01b7cdb86a0f6e268d8d6ec3019e60a3408fd906
JueFan <juefan_c@qq.com>
2014-07-06 22:53:51 +0800

    测试
    测试

---
+++ src/org/juefan/bayes/NaiveBayes.java
@@ -0,0 +1,19 @@
+package org.juefan.bayes;
+
+import java.util.ArrayList;
+
+import org.juefan.basic.BasicData;
+import org.juefan.basic.ClassificationAlg;
+import org.juefan.data.Data;
+
+public class NaiveBayes implements ClassificationAlg{
+
+	@Override
+	public boolean isStop(ArrayList<? extends BasicData> datas) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
